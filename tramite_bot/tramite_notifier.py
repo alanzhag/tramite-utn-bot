c22fde1ea046c578cafce82f631a2188cc3cd840
Alan Zhao <alan.zhao@despegar.com>
2019-03-30 21:46:54 -0300

    now decoding notification

--- tramite_bot/tramite_notifier.py
+++ tramite_bot/tramite_notifier.py
@@ -27,5 +27,5 @@ class TelegramNotifier:
         Id: {}
         Fecha: {}
         Comentario: {}
-        """.format(movement.external_id, movement.raw_datetime, movement.comment.decode("utf-8"))
-        self.notify(textwrap.dedent(message))
+        """.format(movement.external_id, movement.raw_datetime, movement.comment)
+        self.notify(textwrap.dedent(message).decode("utf-8"))
